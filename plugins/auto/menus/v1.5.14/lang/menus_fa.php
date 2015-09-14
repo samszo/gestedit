eb46e8cc7572aaf1eb0e0b385a59e01078fbe418
Samuel Szoniecky <samszon@gmail.com>
2015-09-14 22:27:52 +0200

    new squelette

---
+++ plugins/auto/menus/v1.5.14/lang/menus_fa.php
@@ -0,0 +1,176 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/menus?lang_cible=fa
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'confirmer_supprimer_entree' => 'واقعاً مي‌خواهيد اين مدخل را حذف كنيد؟',
+
+	// D
+	'description_menu_accueil' => 'پيوند به صفحه‌ي پذيرش سايت.',
+	'description_menu_articles_rubrique' => 'نمايش فهرست مقالات يك بخش ',
+	'description_menu_deconnecter' => 'اگر بازديد كننده وصل شد، يك مدخل اضافه كنيد تا قطع شدن را به او ارايه دهد.',
+	'description_menu_espace_prive' => 'پيوندي كه ارتباط با سايت را فعال مي‌كند در صورتي كه قبلاً به سايت وصل نشده باشيد، و بعد وارد قسمت شخصي سايت شويد البته اگر مجاز باشيد.  ',
+	'description_menu_groupes_mots' => 'به طور خودكار گروه‌واژه‌ها و مقالات مرتبط با آن‌ها را فهرست مي‌كند. اين فهرست به صورت پيش‌ گزيده گروه‌‌واژه‌ها و كليدواژه‌هاي تحت آن‌ها را نشان مي‌دهد. اگر اسكلت يك groupes_mots.html وجود داشته باشد، پيوند به آن گروه‌واژه فعال مي‌شود. ',
+	'description_menu_lien' => 'افزودن يك پيوند فرداً تعيين شده، هم دروني (يوآرال نسبي) و هم بروني (http://...)',
+	'description_menu_mapage' => 'اگر بازديد‌كننده متصل است، يك پيوند به صفحه‌ي نويسنده‌‌‌اش اضافه كن.',
+	'description_menu_mots' => 'خودكار يك منوي فهرست تمام مقالات مرتبط با كليدواژه را نشان بده.',
+	'description_menu_objet' => 'ايجاد پيوند به چيزهاي اسپيپي: مقاله، بخش يا چيزهاي ديگر. پيش‌گزيده، هر مدخل تيتر آن چيز را داراست.',
+	'description_menu_page_speciale' => 'افزودن پيوند به صفحه‌ي اسكلت با استفاده از يوآرال اين فرم <code>spip.php?page=nom&param1=xx&param2=yyy...</code>. چنين صفحه‌هايي اغلب توسط پلاگين‌ها استفاده مي‌شوند.  ',
+	'description_menu_page_speciale_zajax' => 'افزودن يك پيوند به يك بلوك در صفحه‌اي قابل دسترس به وسيله‌ي يك يوآرال از نوع <code>spip.php?page=nom&param1=xx&param2=yyy...</code>. اين مستلزم يك استكلت نوع z و يك پلاگين <a href="http://contrib.spip.net/MediaBox">médiabox</a> خواهد بود.  ',
+	'description_menu_rubriques_articles' => 'نمايش يك فهرست از بخش‌ها، و اگر مي‌خواهيد، زيربخش‌ها و مقاله‌ها در چندين سطح. 
+به صورت پيش‌گزيده، نمايش تمام بخش‌ها ريشه، مرتب شده بر اساس تيتر(شماره‌اي و بعد الفبايي). مقاله‌ها به طور نظام‌مند بعد از بخش‌ها قرار مي‌گيرند. 
+',
+	'description_menu_secteurlangue' => 'اين مدخل به سايت‌هايي اختصاص دارد كه يك بخش براي زبان دارند. اين مدخل به صورت خودكار منيويي را نشان مي‌دهد كه بخش‌ها آن قسمت منطبق با آن زبان و صفحه، و اگر بخواهند زيربخش‌ها را تا چندين سطح، را فهرست مي‌كنند. به صورت پيش‌گزيده، تمام بخش‌هاي عد از ريشه را، به ترتيب تيتر (طبق شماره و بعد طبق الفا) نشان مي‌دهد. 
+',
+	'description_menu_texte_libre' => 'فقط متني كه دوست داريد، يا يك كد زباني اسپيپ  (<:...:>)',
+
+	// E
+	'editer_menus_editer' => 'اصلاح اين منيو',
+	'editer_menus_explication' => 'منيوهاي سايتتان را اينجا ايجاد و پيكربندي كنيد.',
+	'editer_menus_exporter' => 'صادر سازي اين منيو',
+	'editer_menus_nouveau' => 'ايجاد يك منيوي جديد',
+	'editer_menus_titre' => 'منيوهاي سايت',
+	'entree_afficher_articles' => 'مقاله‌ها در منيو‌ گنجانده شود؟ (براي اين بگذاريد «بله»)',
+	'entree_afficher_item_suite' => 'گنجاندن مقاله‌ها در منيو؟ (براي اين بگذاريد «بله»)',
+	'entree_articles_max' => 'اگر بله، نمايش مقاله‌ها فقط اگر بخش شامل حداكثر ايكس ايكس مقاله است؟ (عدد مربوط به حداكثر مقاله را بگذاريد، براي نمايش تمام مقاله‌ها، خالي بگذاريد)
+',
+	'entree_articles_max_affiches' => 'در اين صورت، تعداد مقاله‌هاي فهرست شده تا حداكثر xx  محدود شود(بعد از يك فقره «...تمام مقاله‌ها» با يك لينك به بخش‌ والد)؟ (تعداد حداكثر مقاله‌ها را مشخص كنيد ، يا خالي بگذاريد تا تمام مقاله‌ها نشان داده شوند)
+', # MODIF
+	'entree_aucun' => 'هيچي',
+	'entree_bloc' => 'بلاك ازپيپ (Bloc Zpip)',
+	'entree_choisir' => 'نوع مدخلي را كه مي‌خواهيد اضافه كنيد انتخاب نماييد.',
+	'entree_classe_parent' => 'طبقه‌ي پيوند‌هاي عناصر والد. اين طبقه دو باره به li>داراي پيامد  ul / lاضافه خواهد كرد. 
+به عنوان نمونه، اگر شما «ددي» را تايپ كنيد، اين به شما اجازه مي‌دهد تا پلاگين« منيو طوماري 2»» را براي فرمت بخشي به منيو مورد استفاده قرار دهيد. 
+',
+	'entree_connexion_objet' => 'براي ديدن اوبژه وصل بودن («session»وارد كنيد) يا قطع بودن («nosession»وارد كنيد) لازم است',
+	'entree_contenu' => 'محتوا',
+	'entree_css' => 'كلاش‌هاي سي.اس.اس (حامل) مدخل',
+	'entree_css_lien' => 'كلاس‌هاي سي.اس.اس پيوند',
+	'entree_id_groupe' => 'تعداد گروه واژه‌ها',
+	'entree_id_mot' => 'تعداد كليدواژه‌ها',
+	'entree_id_objet' => 'تعداد',
+	'entree_id_rubrique' => 'تعداد بخش‌هاي والد',
+	'entree_id_rubrique_ou_courante' => 'تعداد بخش‌ والد يا «فعلي» اگر بخش‌ والد در متن بخش فعلي باشد.',
+	'entree_id_rubriques_exclues' => 'تعداد بخش‌ها براي كنارگذاري، جداشده با ويرگول',
+	'entree_id_secteur_exclus' => 'تعداد بخش‌ها براي داخل‌ گذاري، جداشده با ويرگول',
+	'entree_infini' => 'تا بي‌نهايت',
+	'entree_mapage' => 'صفحه‌ي شخصي من',
+	'entree_masquer_articles_uniques' => 'در اين صورت و اگر يك بخش فقط يك مقاله دارد، آن را مخفي كند؟ (براي اين كار بگذاريد «بله»)',
+	'entree_niveau' => 'سطح زيربخش‌ها',
+	'entree_nombre_articles' => 'تعداد حداكثر مقاله‌ها (پيش گزيده صفر)',
+	'entree_page' => 'نام صفحه',
+	'entree_parametres' => 'فهرست پارامترها',
+	'entree_rubriques_max_affichees' => 'در اين صورت، تعداد بخش‌هاي فهرست شده تا حداكثر xx محدود شئذ (در پي فقره‌‌ي . . . «تمام بخش‌ها» با پيوند به بخش والد)؟
+(حداكثر تعداد بخش‌ها را تعيين كنيد، براي نشان دادن تمام آن‌ها خالي بگذاريد)
+', # MODIF
+	'entree_sousrub_cond' => 'فقط نمايش زيربخش‌هاي بخش فعلي (بگذاريد «بله»، در غيراينصورت خالي بگذاريد)',
+	'entree_suivant_connexion' => 'منحصر سازي مدخل بعدي به اتصال (بگذاريد «اتصال» براي نمايش فقط اگر بازديد كننده متصل باشد، بگذاريد «قطع» براي مورد عكس، يا خالي بگذاريد تا هميشه نشان داده شود).
+', # MODIF
+	'entree_suivant_connexion_connecte' => 'فقط اگر متصل باشد',
+	'entree_suivant_connexion_deconnecte' => 'فقط اگر قطع باشد',
+	'entree_sur_n_articles' => '@n@ مقاله‌ي منتشره',
+	'entree_sur_n_mots' => '@n@ كلمه‌هاي منتشره ',
+	'entree_sur_n_niveaux' => 'تحت @n@ سطح',
+	'entree_titre' => 'تيتر',
+	'entree_titre_connecter' => 'تيتر براي دستيابي به فرم شناسايي',
+	'entree_titre_prive' => 'تيتر براي دسترسي به قسمت شخصي',
+	'entree_traduction_articles_rubriques' => 'در صورت امكان، نمايش مقاله‌هاي اين بخش به زبان متن («ترجمه» (trad) را براي اينكار بگذاريد)',
+	'entree_traduction_objet' => 'در مور مقاله، ترجمه را در ارتباط با آن متن انتخاب كنيد (براي آن trad را بگذاريد)', # MODIF
+	'entree_type_objet' => 'نوع اوبژه',
+	'entree_url' => 'نشاني (يو.آر.ال) سايت شما',
+	'entree_url_public' => 'نشاني بازگشت بعد از اتصال ',
+	'erreur_aucun_type' => 'هيچ نوع مدخلي يافت نشد.',
+	'erreur_autorisation' => 'شما مجاز به اصلاح منيوي سايت نيستيد.',
+	'erreur_identifiant_deja' => 'از اين شناسه يك منيوي ديگر استفاده مي‌كند',
+	'erreur_identifiant_forme' => 'شناسه بايد فقط از حروف، رقم، و خط‌ زير تركيب شده باشد.',
+	'erreur_menu_inexistant' => 'منيوي درخواستي شماره @id@ موجود نيست.',
+	'erreur_mise_a_jour' => 'هنگام به هنگام سازي پايگاه داده‌ها خطايي رخ داده است.',
+	'erreur_parametres' => 'در پارامتر‌هاي صفحه خطايي وجود دارد',
+	'erreur_type_menu' => 'بايد نوعي از منيو را انتخاب كنيد',
+	'erreur_type_menu_inexistant' => 'اين نوع مينو فعلا/ديگر در دسترس نيست',
+
+	// F
+	'formulaire_ajouter_entree' => 'يك مدخل اضافه كنيد',
+	'formulaire_ajouter_sous_menu' => 'ايجاد يك زيرمنيو',
+	'formulaire_css' => 'سي.اس.اس طبقات',
+	'formulaire_css_explication' => 'مي‌توانيد به منيوي خود طبقات سي.اس.اس تكميلي اضافه كنيد.',
+	'formulaire_deplacer_bas' => 'حركت به پائين',
+	'formulaire_deplacer_haut' => 'حركت به بالا',
+	'formulaire_facultatif' => 'دلبخواهي ',
+	'formulaire_identifiant' => 'شناسه',
+	'formulaire_identifiant_explication' => 'يك كليدواژه‌ي منحصر به فرد بدهيد تا بتوانيد منيوي خود را به راحتي فرابخوانيد.',
+	'formulaire_ieconfig_choisir_menus_a_importer' => 'منيو‌هايي را كه مي‌خواهيد صادر كنيد انخاب كنيد',
+	'formulaire_ieconfig_importer' => 'واردات',
+	'formulaire_ieconfig_menu_meme_identifiant' => 'توجه: يك منيو با همين شناسه در سايت شما موجود است!',
+	'formulaire_ieconfig_menus_a_exporter' => 'منيو‌هاي صادراتي:',
+	'formulaire_ieconfig_ne_pas_importer' => 'وارد نكردن',
+	'formulaire_ieconfig_remplacer' => 'جايگزين سازي منيوي موجود با منوي وارداتي',
+	'formulaire_ieconfig_renommer' => 'تجديد نام اين منيو پيش از واردات',
+	'formulaire_importer' => 'واردسازي يك منيو',
+	'formulaire_importer_explication' => 'اگر يك منيو را داخل يك پرونده صادر كرده باشيد، مي‌توانيد آن را اكنون وارد كنيد.',
+	'formulaire_modifier_entree' => 'اصلاح اين مدخل',
+	'formulaire_modifier_menu' => 'اصلاح منيو:',
+	'formulaire_nouveau' => 'منيوي جديد',
+	'formulaire_partie_construction' => 'ساخت منيو',
+	'formulaire_partie_identification' => 'شناسه‌ي منيو',
+	'formulaire_supprimer_entree' => 'حذف اين مدخل
+
+',
+	'formulaire_supprimer_menu' => 'حذف منيو',
+	'formulaire_supprimer_sous_menu' => 'حذف اين زيرمنيو',
+	'formulaire_titre' => 'تيتر',
+
+	// I
+	'info_afficher_articles' => 'مقاله‌ها در منيو گنجانده خواهند شد',
+	'info_articles_max' => 'فقط اگر بخش بيش از @max@مقاله داشته باشد',
+	'info_articles_max_affiches' => 'نمايش محدود به @max@ مقاله ',
+	'info_classe_parent' => 'طبقه‌ي عناصر والد‌:',
+	'info_connexion_obligatoire' => 'اتصال اجباري',
+	'info_deconnexion_obligatoire' => 'فقط هنگام قطعي',
+	'info_masquer_articles_uniques' => 'منحصراً مقاله‌هاي پنهان',
+	'info_numero_menu' => 'منيوي شماره‌ي: ',
+	'info_page_speciale' => 'پيوند به صفحه‌ي  «@page@»',
+	'info_page_speciale_zajax' => 'مدال باكس صفحه‌ي  «@page@» براي بلوك «@bloc@»   ',
+	'info_rubrique_courante' => 'بخش فعلي',
+	'info_rubriques_exclues' => '/به جز بخش‌(هاي)@id_rubriques@ ',
+	'info_rubriques_max_affichees' => 'نمايش محدود به @max@ بخش‌ها',
+	'info_secteur_exclus' => '/به جز بخ‌ش (هاي) @id_secteur@',
+	'info_sousrub_cond' => 'فقط زيربخش‌هاي بخش‌هاي فعلي نمايش داده مي‌شوند.',
+	'info_tous_groupes_mots' => 'تمام گروه‌ واژه‌ها',
+	'info_traduction_recuperee' => 'متن ترجمه‌ي منتخب را تعيين خواهد كرد',
+	'info_tri' => 'ترتيب بخش‌ها:‌',
+	'info_tri_alpha' => '(الفبايي)',
+	'info_tri_articles' => 'مرتب‌ سازي مقاله‌ها:',
+	'info_tri_num' => '(شماره‌اي)',
+
+	// N
+	'noisette_description' => 'گنجاندن منيوي تعريف شده با پلاگين منو‌ها.',
+	'noisette_label_afficher_titre_menu' => 'نمايش تيتر منيو؟',
+	'noisette_label_identifiant' => 'منيو براي نمايش: ',
+	'noisette_nom_noisette' => 'منيو',
+	'nom_menu_accueil' => 'پذيرش',
+	'nom_menu_articles_rubrique' => 'مقاله‌هاي يك بخش',
+	'nom_menu_deconnecter' => 'قطع شدن',
+	'nom_menu_espace_prive' => 'اتصال/ پيوند به قسمت شخصي',
+	'nom_menu_groupes_mots' => 'كليدواژه‌ها و مقاله‌هاي يك گروه‌ واژه',
+	'nom_menu_lien' => 'پيوند دلبخواهي',
+	'nom_menu_mapage' => 'صفحه‌ من',
+	'nom_menu_mots' => 'مقاله‌هاي يك كليد واژه',
+	'nom_menu_objet' => 'مقاله، بخش يا ساير چيزهاي اسپيپي ',
+	'nom_menu_page_speciale' => 'پيوند به يك صفحه‌ي اسكلت',
+	'nom_menu_page_speciale_zajax' => 'يك بلوك از صفحه‌ي ازپيپ',
+	'nom_menu_rubriques_evenements' => 'برنامه‌هاي مربوط به بخش‌ها',
+	'nom_menu_secteurlangue' => 'بخش‌هاي زباني ',
+	'nom_menu_texte_libre' => 'متن آزاد',
+
+	// T
+	'tous_les_articles' => '...تمام مقاله‌ها',
+	'toutes_les_rubriques' => '...تمام بخش‌ها'
+);
+
+?>
