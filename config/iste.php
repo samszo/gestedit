c8d550dee7fa3eccf01e2e8816b2cb42f52a1c74
RochDLY <43650596+RochDLY@users.noreply.github.com>
2019-03-05 12:09:23 +0100

    rd

--- config/iste.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!defined("_ECRIRE_INC_VERSION")) return;
-define('_MYSQL_SET_SQL_MODE',true);
-spip_connect_db('localhost','','root','','iste','mysql', '');
-mysql_query("SET NAMES 'utf8'");
-?>
