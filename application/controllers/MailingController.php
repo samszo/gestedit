fc8de0d18b212982fa5d89583e648cc5234a40c3
RochDLY <43650596+RochDLY@users.noreply.github.com>
2019-03-10 13:34:56 +0100

    maj

--- application/controllers/MailingController.php
+++ application/controllers/MailingController.php
@@ -12,7 +12,7 @@ class MailingController extends Zend_Controller_Action
         $s->bTrace = false;
         $s->trace($rs);
         $this->view->jsNomen = $rs;
-/*
+
         // Affichage prospects dans index mailing
         $dbProsp = new Model_DbTable_Iste_prospect();
         $rs = json_encode($dbProsp->getAll());
@@ -28,7 +28,96 @@ class MailingController extends Zend_Controller_Action
         $s->bTrace = false;
         $s->trace($rs);
         $this->view->jsEtab = $rs;
-*/
+
     }
 
+    public function insertAction()
+    {
+        switch ($this->_getParam('obj')) {
+            case 'prospect':
+                $dbP = new Model_DbTable_Iste_prospect();
+                $dbP->ajouter(array('nom_prenom'=>$this->_getParam('nom_prenom'), 'email_prospect'=>$this->_getParam('email_prospect'), 'email2_prospect'=>$this->_getParam('email2_prospect'), 'etab_prospect'=>$this->_getParam('etab_prospect'), 'langue_prospect'=>$this->_getParam('langue_prospect'), 'clientIste_prospect'=>$this->_getParam('clientIste_prospect'), 'membreEdito_prospect'=>$this->_getParam('membreEdito_prospect')));
+                break;
+            case 'etab':
+                $dbE = new Model_DbTable_Iste_etab();
+                $rs = $dbE->ajouter(array('url_labo_etab'=>$this->_getParam('url_labo_etab'), 'adresse_etab'=>$this->_getParam('adresse_etab'), 'ville_etab'=>$this->_getParam('ville_etab'), 'cp_etab'=>$this->_getParam('cp_etab'), 'pays_etab'=>$this->_getParam('pays_etab'), 'responsableLabo_etab'=>$this->_getParam('responsableLabo_etab'), 'affiliation1_etab'=>$this->_getParam('affiliation1_etab'), 'affiliation2_etab'=>$this->_getParam('affiliation2_etab'), 'origine_etab'=>$this->_getParam('origine_etab')));
+                break;
+            case 'nomenclature':
+                $dbN = new Model_DbTable_Iste_nomenclature();
+                $rs = $dbP->ajouter(array('label'=>$this->_getParam('label'),'code'=>$this->_getParam('code')));
+                break;
+            default:
+                # code...
+                break;
+        }
+
+        $this->view->rs = $rs;
+    }
+
+
+    public function deleteAction()
+    {
+        $this->initInstance();
+            
+        //récupère les paramètres
+        $id = $this->_getParam('id');
+        //création de l'objet BDD
+        $oName = "Model_DbTable_Iste_".$this->_getParam('obj');
+        $oBdd = new $oName();
+        //traitement des supressions
+        switch ($this->_getParam('obj')) {						
+            case 'prospectxetab':
+                $r = $oBdd->remove($this->_getParam("id_etab"),$this->_getParam("id_prospect"));
+            break;			
+            case 'prospectxnomenclature':
+                $r = $oBdd->remove($this->_getParam("id_nomenclature"),$this->_getParam("id_prospect"));
+            break;						
+            default:
+                $r = $oBdd->remove($id);
+            break;
+            }    		    	
+            $this->view->rs = $r;
+            if($r["message"])$this->view->message = $r["message"]; 
+            else $this->view->message = "Les données sont supprimées.";
+            $dbHM = new Model_DbTable_Iste_histomodif();
+            $dbHM->ajouter(array("id_uti"=>$this->ssUti->uti["id_uti"],"action"=>__METHOD__,"obj"=>$this->_getParam('obj'),"id_obj"=>$id));   		
+            
+    }
+    
+    public function updateAction()
+    {
+    		$this->initInstance();
+    	
+    		//récupère les paramètres
+    		$params = $this->_request->getParams();
+    		$id = $this->_getParam('recid');
+    		$obj = $this->_getParam('obj');
+    		//création de l'objet BDD
+    		$oName = "Model_DbTable_Iste_".$obj;
+    		$oBdd = new $oName();
+    		//enlève les paramètres Zend
+    		unset($params['controller']);
+    		unset($params['action']);
+    		unset($params['module']);
+    		//et les paramètres de l'ajout
+    		unset($params['obj']);
+    		unset($params['recid']);
+    		//traitement des boolean
+    		if($params['pdf'])$params['pdf']=="true" ? $params['pdf']=1 : $params['pdf']=0;  				    			
+    		//traitement des sauvegardes
+		switch ($this->_getParam('obj')) {
+			case 'prospectxetab':
+				$oBdd->edit($params["id_prospect"],$params["id_etab"]);
+			break;			
+			case 'prospectxnomenclature':
+                $oBdd->edit($params["id_prospect"],$params["id_etab"]);
+			break;								
+			default:
+				$this->view->rs = $oBdd->edit($id,$params);
+			break;
+		} 
+		if(!$this->view->message)$this->view->message = "Les modifications ont été effectuées.";
+		$dbHM = new Model_DbTable_Iste_histomodif();
+		$dbHM->ajouter(array("id_uti"=>$this->ssUti->uti["id_uti"],"action"=>__METHOD__,"obj"=>$this->_getParam('obj'),"id_obj"=>$id,"data"=>json_encode($params)));   		
+    }
 }
