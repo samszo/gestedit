02874da8d9a6d181dd4229115633d1b0ab1ea499
RochDLY <roch.delannay@gmail.com>
2019-04-02 15:43:27 +0200

    update import mailing

---
+++ library/Flux/Mailing.php
@@ -0,0 +1,52 @@
+<?php
+/**
+* Class pour gérer les flux de mailing
+*
+*
+* @author     Roch Delannay
+* @license    CC0 1.0 Universal (CC0 1.0) Public Domain Dedication http://creativecommons.org/publicdomain/zero/1.0/ 
+*/
+class Flux_Mailing extends Flux_Site{
+        
+    function __construct($idBase=false,$trace=false){    	
+    	    	
+			parent::__construct($idBase,$trace);
+    		
+    }
+	
+	
+    /**
+	* méthode pour importer un fichier de vente
+	*
+    * @param string 		$path
+    * @param integer		$idFic
+    * @param integer		$anFin
+	* @param integer		$nbOnglet
+    * 
+    */
+    public function importer($path, $idFic=false){
+		$this->trace("DEB ".__METHOD__);
+		$this->data = new csvToArray();
+		
+		if($idFic){
+			$rsFic = $this->dbFic->findById_importfic($idFic);
+			$path = str_replace(WEB_ROOT, ROOT_PATH, $rsFic["url"]);
+			$this->trace("//url du fichier = ".$path);
+		}		
+		
+		$this->data->read($path);
+		
+		$ref = str_replace("\0", "", $this->data->sheets[0]['cells'][1][1]);
+
+		$descColo = array("col1","col2","col3","col4","col5","col6");	
+		
+		if($idFic){
+			$this->dbFic->edit($idFic,array("reference"=>$ref,"coldesc"=>json_encode($descColo)
+				));
+		}else{
+			$idFic = $this->dbFic->ajouter(array("url"=>$path,"type"=>"Mailing","coldesc"=>json_encode($descColo)
+				,"reference"=>$ref));
+		}
+		$this->trace("fichier enregistré");
+	}
+}
